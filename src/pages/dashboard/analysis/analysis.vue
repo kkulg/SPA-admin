405c5981c5d339522516f5a3762df7eec0ab7127
cos <kkulg0922@163.com>
2021-05-20 22:54:45 +0800

    feat(example): add page example

--- src/pages/dashboard/analysis/analysis.vue
+++ src/pages/dashboard/analysis/analysis.vue
@@ -1,10 +1,55 @@
 <template>
-  <div>LcAnalysis</div>
+  <el-row :gutter="20">
+    <el-col :span="6">
+      <div class="grid-content">
+        <el-skeleton style="width: 100%"
+                     animated>
+          <template #template>
+            <el-skeleton-item variant="image"
+                              style="width: 100%; height: 240px;" />
+            <div style="padding: 14px;">
+              <el-skeleton-item variant="h3"
+                                style="width: 50%;" />
+              <div style="display: flex; align-items: center; justify-items: space-between; margin-top: 16px; height: 16px;">
+                <el-skeleton-item variant="text"
+                                  style="margin-right: 16px;" />
+                <el-skeleton-item variant="text"
+                                  style="width: 30%;" />
+              </div>
+            </div>
+          </template>
+        </el-skeleton>
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content"></div>
+    </el-col>
+  </el-row>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { ElRow, ElCol, ElSkeleton, ElSkeletonItem } from 'element-plus';
 
 export default defineComponent({
-  name: 'LcAnalysis'
+  name: 'LcAnalysis',
+  components: {
+    ElRow,
+    ElCol,
+    ElSkeleton,
+    ElSkeletonItem
+  }
 });
 </script>
+<style lang="scss" scoped>
+.grid-content {
+  background: #fff;
+  padding: 20px;
+  border-radius: 4px;
+}
+</style>
--- src/pages/dashboard/monitor/monitor.vue
+++ src/pages/dashboard/monitor/monitor.vue
@@ -1,10 +1,67 @@
 <template>
-  <div>LcMonitor</div>
+  <el-row :gutter="20">
+    <el-col :span="6">
+      <div class="grid-content">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+               class="image">
+          <div style="padding: 14px;">
+            <span>好吃的汉堡</span>
+            <div class="bottom">
+              <el-button type="text"
+                         class="button">操作按钮</el-button>
+            </div>
+          </div>
+        </el-card>
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content"></div>
+    </el-col>
+  </el-row>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { ElRow, ElCol, ElCard, ElButton } from 'element-plus';
 
 export default defineComponent({
-  name: 'LcMonitor'
+  name: 'LcMonitor',
+  components: {
+    ElRow,
+    ElCol,
+    ElCard,
+    ElButton
+  }
 });
 </script>
+<style lang="scss" scoped>
+.grid-content {
+  background: #fff;
+  padding: 20px;
+  border-radius: 4px;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  padding: 0;
+  min-height: auto;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+</style>
