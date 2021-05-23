3dfa68ad76460e1148a255c42010c13d5f4da765
cos <kkulg0922@163.com>
2021-05-23 22:36:00 +0800

    feat(remove server code): remove server code

--- server/src/users/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class CreateUserDto {
-  @IsEmail()
-  @IsNotEmpty()
-  readonly username: string;
-  @IsNotEmpty()
-  readonly password: string;
-}
